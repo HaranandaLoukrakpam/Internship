b9f35b59f939a79b6e31c73a8aafcc22443eac14
Harananda <haranandaloukrakpam2@gmail.com>
2026-07-16 17:24:29 +0530

    commit for assignment

---
+++ JavaScript_Assignment/part5.js
@@ -0,0 +1,16 @@
+let x = 10;
+let y = '10';
+console.log(x == y);  // true
+console.log(x === y); // false
+ 
+
+let arr1 = [1, 2, 3];
+arr1.push(4);
+arr1.pop();
+console.log(arr1); // [1, 2, 3]
+ 
+
+let arr2 = ['A', 'B', 'C'];
+arr2[1] = 'Z';
+console.log(arr2); // ['A', 'Z', 'C']
+ 
